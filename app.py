4d37456308ac69aa16f07e0c733eb465eba313f6
jhgan0159 <jhgan0159@gmail.com>
2026-05-02 21:25:31 +0800

    Phase1

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ def pendulum():
 def tingkatan4experiment2():
     return render_template('Tingkatan4eksperimen2.html')
 
+@app.route('/form4experiment3')
+def form4experiment3():
+    return render_template('form4eksperimen3.html')
+
+@app.route('/form4experiment4')
+def form4experiment4():
+    return render_template('form4eksperimen4.html')
+
 # The address for your Board Game
 @app.route('/boardgame')
 def play_game():
@@ -29,6 +37,10 @@ def play_game():
 @app.route('/teacher-admin')
 def admin_panel():
     return render_template('admin.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
